33ad12510bbb6207bf2a5440cfdc41f969f9ffce
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-02 23:03:16 -0600

    Behavior Parametrization - 3
    (Java in action)

--- src/BehaviorParametrization.java
+++ src/BehaviorParametrization.java
@@ -5,6 +5,9 @@ class  Apple {
     public String getColor(){
         return "color";
     }
+    public Integer getWeight() {
+        return 1;
+    }
 }
 
 public class BehaviorParametrization {
@@ -25,6 +28,18 @@ public class BehaviorParametrization {
 
 //    List<Apple>greenApples = filterApplesByColor(inventory, GREEN);
 //    List<Apple>redApples = filterApplesByColor(inventory, RED);
+
+    // now we create a new method to filter the apples by weight, but
+    // this new method breaks the DRY principle since repeat code form our initial method `filterApplesByColor`
+    public static List<Apple> filterApplesByWeight(List<Apple>inventory, int weight) {
+        List<Apple>result = new ArrayList<>();
+        for (Apple apple: inventory){
+            if ( apple.getWeight() > weight) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
 
     // when you find yourself writing nearly repeated code, try to abstract instead.
 }
