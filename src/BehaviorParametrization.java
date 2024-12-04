26729b814e3c89a379f415bfadbbb8f85ecf4166
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-03 18:33:47 -0600

    Behavior Parametrization - 4
    relocate the classes out of BehaviorParametrization class just to improve readability
    
    (Java in action)

--- src/BehaviorParametrization.java
+++ src/BehaviorParametrization.java
@@ -11,43 +11,34 @@ class  Apple {
     }
 }
 
+enum Color { RED, GREEN };
+
 // let define a contract to model the selection criteria (predicate)
 interface ApplePredicate{
     boolean test (Apple apple);
 }
 
+class AppleHeavyWeightPredicate implements ApplePredicate {
+    public boolean test(Apple apple) {
+        return apple.getWeight() >150;
+    }
+}
+
+class AppleGreenColorPredicate implements ApplePredicate {
+    public boolean test(Apple apple) {
+        return Color.GREEN.equals(apple.getColor());
+    }
+}
+
+// now we can create multiple predicates and pass them to our filter function
+class AppleRedAndHeavyPredicate implements ApplePredicate {
+    public boolean test(Apple apple){
+        return Color.RED.equals(apple.getColor())
+                &&apple.getWeight() >150;
+    }
+}
+
 public class BehaviorParametrization {
-    enum Color { RED, GREEN };
-
-   // Third attempt: filtering with every attribute you can think of
-
-   public class AppleHeavyWeightPredicate implements ApplePredicate {
-       public boolean test(Apple apple) {
-           return apple.getWeight() >150;
-       }
-   }
-    public class AppleGreenColorPredicate implements ApplePredicate {
-        public boolean test(Apple apple) {
-            return Color.GREEN.equals(apple.getColor());
-        }
-    }
-
-    // now we can create multiple predicates and pass them to our filter function
-    public class AppleRedAndHeavyPredicate implements ApplePredicate {
-        public boolean test(Apple apple){
-            return Color.RED.equals(apple.getColor())
-                    &&apple.getWeight() >150;
-        }
-    }
-
-    /**
-     * This is what behavior parameterization means: the ability to tell a method
-     * to take multiple behaviors (or strategies) as parameters and use them internally
-     * to accomplish different behaviors.
-     */
-
-    // attempt fourth: now our method receive a predicate instead of multiple parameters
-    // (filtering by abstract criteria)
     public static List<Apple> filterApples(List<Apple> inventory,
                                            ApplePredicate p) {
         List<Apple> result = new ArrayList<>();
@@ -58,10 +49,5 @@ public class BehaviorParametrization {
         }
         return result;
     }
-
-    // ------------- Usage of the new Predicate -------------
-//    List<Apple>redAndHeavyApples =
-//            filterApples(inventory, new AppleRedAndHeavyPredicate());
-
 }
 
